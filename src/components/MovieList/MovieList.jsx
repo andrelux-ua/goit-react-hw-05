dcc2bd34c1975dcec22e8f04e340322f531567f4
Andriy Kiselyov <168927164+andrelux-ua@users.noreply.github.com>
2025-03-21 21:45:35 +0200

    searchmovies-1

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -18,17 +18,16 @@ const MovieList = () => {
   }, []);
 
   return (
-    <div className="p-4">
-      <h2 className="text-xl font-bold">Popular Movies</h2>
-      <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
+    <div>
+      <h2>Popular Movies</h2>
+      <div>
         {movies.map(movie => (
-          <div key={movie.id} className="p-2 bg-gray-800 rounded-lg">
+          <div key={movie.id}>
             <img
               src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
               alt={movie.title}
-              className="w-full rounded-md"
             />
-            <h3 className="text-white mt-2">{movie.title}</h3>
+            <h3>{movie.title}</h3>
           </div>
         ))}
       </div>
---
+++ src/components/SearchMovies/SearchMovies.jsx
@@ -0,0 +1,13 @@
+import { searchMovies } from '../../movieServis';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+
+function SearchMovies() {
+  return (
+    <>
+      <Formik></Formik>
+    </>
+  );
+}
+
+export default SearchMovies;
